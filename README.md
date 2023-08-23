1c9ea037b6dee5ec27b33ce755be68f5d6229082
Oliver <oliver.ladores@wvsu.edu.ph>
2023-08-23 21:40:11 +0800

    Implemented depth first search and added README.

---
+++ README.md
@@ -0,0 +1,8 @@
+# [CCS246] Implementing Depth-First Search with Networkx
+
+Instructions to run:
+1. Clone this repo `git clone <repo-link>`.
+2. Run python venv `py -m venv .`.
+3. Activate virtual environment `Scripts\activate.bat`.
+4. Run script `py depth_first_search\__init__.py`.
+
--- depth_first_search/__init__.py
+++ depth_first_search/__init__.py
@@ -1,5 +1,8 @@
-import networkx as nx
+"""This module showcases an implementation of depth-first search with networkx. """
+
 import matplotlib.pyplot as plt
+import networkx as nx
+
 
 def main():
     third_district = nx.Graph()
@@ -19,7 +22,31 @@ def main():
     plt.figure("Third District Graph")
     nx.draw_networkx(third_district, node_color = "#ced495", node_shape = "s")
     plt.show()
-    print(third_district.edges)
+    # Find a path using depth first search from Pototan to Calinog
+    if third_district_subgraph := depth_first_search(third_district, "Pototan", "Calinog"):
+        nx.draw_networkx(third_district_subgraph, node_color = "#ced495", node_shape = "s")
+        plt.show()
+
+def depth_first_search(graph: nx.Graph, start, target) -> nx.Graph | None:
+    # Copied from https://github.com/TheAlgorithms/Python/blob/master/graphs/depth_first_search.py
+    visited, stack = set(start), [start]
+    step = 0
+    
+    while stack:
+        current = stack.pop()
+        step += 1
+        print(f"Step: {step} @ Node: {current}")
+        visited.add(current)
+        if (current == target):
+            def nodes_filter(x):
+                return x in visited
+            return nx.subgraph_view(graph, nodes_filter)
+        for adjacent in (graph[current]):
+            if adjacent not in visited:
+                stack.append(adjacent)
+        print(stack)
+    
+    return None
 
 if __name__ == "__main__":
     main()
